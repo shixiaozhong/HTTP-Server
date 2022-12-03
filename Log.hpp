aa5c20b48105a896f8c5771632b4369d13a5d248
shixiaozhong <257988550@qq.com>
2022-12-03 18:20:22 +0800

    simple log

--- Log.hpp
+++ Log.hpp
@@ -1,1 +1,22 @@
+// 日志
 #pragma once
+
+#include <iostream>
+#include <string>
+#include <ctime>
+
+#define INFO 1
+#define WARRING 2
+#define ERROR 3
+#define FATAL 4
+#define LOG(level, message) Log(#level, message, __FILE__, __LINE__)
+
+// 日志的格式: [日志级别][时间戳][日志信息][错误文件名称][错误的行数]
+void Log(std::string level, std::string message, std::string file_name, int line)
+{
+    std::cout << "[" << level << "]"
+              << "[" << time(nullptr) << "]"
+              << "[" << message << "]"
+              << "[" << file_name << "]"
+              << "[" << line << "]" << std::endl;
+}
